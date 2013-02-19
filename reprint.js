1bc16f9858bad7576ab09c59ddac85c09990c5f4
vazha-asatiani <vazha.asatiani@jumpstart.ge>
2013-02-18 20:10:06 -0800

    Create reprint.js

---
+++ reprint.js
@@ -0,0 +1,27 @@
+(function ($)
+{
+
+  $.fn.reprint = function ()
+  {
+    $(this).replaceWith(function ()
+    {
+      return $(this)
+        .clone()
+        .wrap('<p class="temp_wrapper">')
+        .parent()
+        .find('img, script, link, iframe[src]')
+        .each(function ()
+          {
+            var el = $(this);
+            var attrname = $(el).is('link') ? 'href' : 'src';
+            var src = el.attr(attrname);
+            src = (src.replace(/(\?|&)_t=[0-9]+&?/, '$1') + ((src.indexOf('?') === -1) ? '?' : '&') + '_t=' + +new Date()).replace(/&&/, '&').replace(/\?&/, '?');
+            el.attr(attrname, src);
+          })
+        .closest('p.temp_wrapper')
+        .html();
+    });
+    return $(this);
+  }
+
+})(jQuery);
