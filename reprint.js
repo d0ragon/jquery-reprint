e9e43511ab90fa9a386f97cd448a22e1bd5b4236
vazha-asatiani <vazha.asatiani@jumpstart.ge>
2013-02-21 15:41:03 +0400

    the code was not working for elements which did not contain images,scripts,links or iframes. fixed that. also changed temp wrapper from p tag to div tag

--- reprint.js
+++ reprint.js
@@ -5,21 +5,18 @@
   {
     $(this).replaceWith(function ()
     {
-      return $(this)
-        .clone()
-        .wrap('<p class="temp_wrapper">')
-        .parent()
-        .find('img, script, link, iframe[src]')
-        .each(function ()
-          {
-            var el = $(this);
-            var attrname = $(el).is('link') ? 'href' : 'src';
-            var src = el.attr(attrname);
-            src = (src.replace(/(\?|&)_t=[0-9]+&?/, '$1') + ((src.indexOf('?') === -1) ? '?' : '&') + '_t=' + +new Date()).replace(/&&/, '&').replace(/\?&/, '?');
-            el.attr(attrname, src);
-          })
-        .closest('p.temp_wrapper')
-        .html();
+      var wrapper = $(this).clone().wrap('<div class="temp_wrapper">').parent();
+
+      wrapper.find('img, script, link, iframe[src]').each(function ()
+      {
+        var el = $(this);
+        var attrname = $(el).is('link') ? 'href' : 'src';
+        var src = el.attr(attrname);
+        src = (src.replace(/(\?|&)_t=[0-9]+&?/, '$1') + ((src.indexOf('?') === -1) ? '?' : '&') + '_t=' + +new Date()).replace(/&&/, '&').replace(/\?&/, '?');
+        el.attr(attrname, src);
+      })
+
+      return wrapper.html();
     });
     return $(this);
   }
